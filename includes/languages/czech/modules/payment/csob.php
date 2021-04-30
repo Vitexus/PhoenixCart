1350f5c075dae200788ed3663ebb70eb4a0e701a
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-05-01 01:22:47 +0200

    Czech flag and csob payment localization update

--- includes/languages/czech/modules/payment/csob.php
+++ includes/languages/czech/modules/payment/csob.php
@@ -12,10 +12,13 @@
  */
 
 const MODULE_PAYMENT_CSOB_TEXT_TITLE = 'CSOB PayGW';
-const MODULE_PAYMENT_CSOB_TEXT_PUBLIC_TITLE = 'Credit Card';
+const MODULE_PAYMENT_CSOB_TEXT_PUBLIC_TITLE = 'Kredu Card';
 const MODULE_PAYMENT_CSOB_TEXT_DESCRIPTION = '<img src="images/icon_info.gif" border="0" />&nbsp;<a href="http://library.oscommerce.com/Package&en&csob&oscom23&csob_js" target="_blank" style="text-decoration: underline; font-weight: bold;">View Online Documentation</a><br /><br /><i class="fas fa-external-link-alt"></i>&nbsp;<a href="https://platebnibrana.csob.cz/" target="_blank" style="text-decoration: underline; font-weight: bold;">Visit CSOB Payments Website</a>';
 
-const MODULE_PAYMENT_CSOB_ERROR_ADMIN_PHP = 'The minimum PHP version this module supports is %s and will not load until the webserver has been installed with a newer version.';
-const MODULE_PAYMENT_CSOB_ERROR_ADMIN_PHP_EXTENSIONS = 'This module requires the following PHP extensions and will and will not load until PHP has been updated:<br /><br />%s';
-const MODULE_PAYMENT_CSOB_ERROR_ADMIN_MERCHANT_ACCOUNTS = 'This module will not load until a merchant account has been defined for the %s currency.';
-const MODULE_PAYMENT_CSOB_ERROR_ADMIN_CONFIGURATION = 'This module will not load until the Merchant ID, Public Key, Private Key, and Client Side Encryption Key parameters have been configured. Please edit and configure the settings of this module.';
+const MODULE_PAYMENT_CSOB_ERROR_ADMIN_PHP = 'Minimální verze php podporovaná modulem je  %s and will not load until the webserver has been installed with a newer version.';
+const MODULE_PAYMENT_CSOB_ERROR_ADMIN_PHP_EXTENSIONS = 'Tento modul vyžaduje PHP rozšíření a nenačte se dokud se php neaktulizuje:<br /><br />%s';
+const MODULE_PAYMENT_CSOB_ERROR_ADMIN_MERCHANT_ACCOUNTS = 'Tento modul se nenačte dokud nebude definováno číslo obchodníka  pro měnu %s.';
+const MODULE_PAYMENT_CSOB_ERROR_ADMIN_CONFIGURATION = 'Tento modul se nenačte dokud nebude definováno číslo obchodníka , Veřejný Klíč, Soukromý klíč a nastavení šifrování nebude configurováno. Je třeba aktualizovat nastavení modulu.';
+
+const MODULE_PAYMENT_CSOB_ERROR_TITLE = 'Chyba platby';
+const MODULE_PAYMENT_CSOB_ERROR_GENERAL = 'Platba expirovala';
