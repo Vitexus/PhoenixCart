89f52d320afaad8e43e8d67d27933ddfac846d68
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-06-02 14:54:26 +0200

    Perform payment check everytime

--- includes/hooks/shop/checkout_payment_stage/csob.php
+++ includes/hooks/shop/checkout_payment_stage/csob.php
@@ -1,7 +1,5 @@
 <?php
 
-#http://phoenix/checkout_payment.php?error_message=Vyberte%2Bzp%C5%AFsob%2Bplatby%2BVa%C5%A1%C3%AD%2Bobjedn%C3%A1vky.&ceid=74nt479h4qr44krdtsd5c2dioa
-
 /*
   $Id$
 
@@ -13,25 +11,40 @@
   Released under the GNU General Public License
  */
 
-$cartPayId = \Ease\WebPage::getRequestValue('payId');
-$cardDttm = \Ease\WebPage::getRequestValue('dttm');
+class hook_shop_checkout_payment_stage_csob {
 
-if ($cartPayId && $cardDttm) { //PayGW Responsed
-    $merchantData = json_decode(trim(base64_decode(\Ease\WebPage::getRequestValue('merchantData'))), 'true');
-    $paymentStatus = \Ease\WebPage::getRequestValue('paymentStatus', 'int');
-    $resultCode = \Ease\WebPage::getRequestValue('resultCode', 'int');
+    public $version = '1.0.0';
 
-    $GLOBALS['order_id'] = $merchantData['orderId'];
-    $GLOBALS['order'] = Guarantor::ensure_global('PureOSC\Order', array_key_exists('order_id', $GLOBALS) ? $GLOBALS['order_id'] : null);
+    public function __construct() {
+        $this->listen_injectSiteStart();
+    }
 
-    $payment_modules = new \payment($merchantData['payment']);
-    $payment_modules->update_status();
+    public function listen_injectSiteStart() {
+
+        $cartPayId = \Ease\WebPage::getRequestValue('payId');
+        $cardDttm = \Ease\WebPage::getRequestValue('dttm');
+
+        if ($cartPayId && $cardDttm) { //PayGW Responsed
+            $merchantData = json_decode(trim(base64_decode(\Ease\WebPage::getRequestValue('merchantData'))), 'true');
+            $paymentStatus = \Ease\WebPage::getRequestValue('paymentStatus', 'int');
+            $resultCode = \Ease\WebPage::getRequestValue('resultCode', 'int');
+
+            $GLOBALS['order_id'] = $merchantData['orderId'];
+            $GLOBALS['order'] = Guarantor::ensure_global('PureOSC\Order', array_key_exists('order_id', $GLOBALS) ? $GLOBALS['order_id'] : null);
+
+            $payment_modules = new \payment($merchantData['payment']);
+            $payment_modules->update_status();
 
 //    if (( is_array($payment_modules->modules) && (count($payment_modules->modules) > 1) && isset(${$_SESSION['payment']}) && !is_object(${$_SESSION['payment']}) ) || (is_object(${$_SESSION['payment']}) && (${$_SESSION['payment']}->enabled == false))) {
 //        tep_redirect(tep_href_link('checkout_payment.php', 'error_message=' . urlencode(ERROR_NO_PAYMENT_MODULE_SELECTED), 'SSL'));
 //    }
-    if (is_array($payment_modules->modules)) {
-        $payment_modules->pre_confirmation_check();
-    }
-}
+            if (is_array($payment_modules->modules)) {
+                $payment_modules->pre_confirmation_check();
+            }
+        } else {
 
+#http://phoenix/checkout_payment.php?error_message=Vyberte%2Bzp%C5%AFsob%2Bplatby%2BVa%C5%A1%C3%AD%2Bobjedn%C3%A1vky.&ceid=74nt479h4qr44krdtsd5c2dioa
+        }
+    }
+
+}
