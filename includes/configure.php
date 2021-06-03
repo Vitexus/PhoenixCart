1615a1b3ab4f217fb2c314dd34a57bebb8367cac
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-06-03 03:58:46 +0200

    Upstream applyed

--- includes/configure.php
+++ includes/configure.php
@@ -2,22 +2,22 @@
 // set the level of error reporting
   error_reporting(E_ALL);
 
-  const HTTP_SERVER = 'http://ph.local';
+  const HTTP_SERVER = 'http://phoenix';
   const COOKIE_OPTIONS = [
     'lifetime' => 0,
-    'domain' => 'ph.local',
+    'domain' => 'phoenix',
     'path' => '/',
     'samesite' => 'Lax',
   ];
   const DIR_WS_CATALOG = '/';
 
-  const DIR_FS_CATALOG = '/home/f/git/PhoenixCart/';
+  const DIR_FS_CATALOG = '/home/vitex/Projects/PureHTML/PhoenixCart/';
 
   date_default_timezone_set('Europe/Prague');
 
 // If you are asked to provide configure.php details
 // please remove the data below before sharing
   const DB_SERVER = 'localhost';
-  const DB_SERVER_USERNAME = 'ph';
-  const DB_SERVER_PASSWORD = 'ph';
-  const DB_DATABASE = 'PhoenixCart';
+  const DB_SERVER_USERNAME = 'phoenix';
+  const DB_SERVER_PASSWORD = 'phoenix';
+  const DB_DATABASE = 'phoenix';
